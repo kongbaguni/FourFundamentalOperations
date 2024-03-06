18c703dad0c4e60945b8694ed4cf6dc1ca3da544
서창열 <kongbaguni@gmail.com>
2024-03-06 14:23:52 +0900

    Initial Commit

---
+++ FourFundamentalOperations/FourFundamentalOperationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourFundamentalOperationsApp.swift
+//  FourFundamentalOperations
+//
+//  Created by Changyeol Seo on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FourFundamentalOperationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
