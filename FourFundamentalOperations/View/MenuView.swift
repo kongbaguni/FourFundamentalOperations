61241175be8a409bc6e4ad2eff3c575cffe3d0e8
서창열 <kongbaguni@gmail.com>
2024-03-12 17:24:24 +0900

    FireAuth 구현

--- FourFundamentalOperations/View/MenuView.swift
+++ FourFundamentalOperations/View/MenuView.swift
@@ -9,21 +9,50 @@ import Foundation
 import SwiftUI
 
 struct MenuView : View {
+    @State var isLogin = false
+    
     var body: some View {
         VStack {
-            ButtonView(
-                image: .init(systemName: "apple.logo"),
-                title: .init("Sign in with Apple"),
-                style: .secondary) {
-            }
-            ButtonView(
-                image: .init("GoogleLogo"),
-                title: .init("Sign in with Google"),
-                style: .secondary) {
-                    
+            if isLogin {
+                Text(AuthManager.shared.auth.currentUser?.uid ?? "")
+                ButtonView(image: .init(systemName: "rectangle.portrait.and.arrow.right"), title: .init("Sign Out"), style: .secondary) {
+                    if AuthManager.shared.signout() == nil {
+                        isLogin = false
+                    }
                 }
+            } else {
+                ButtonView(
+                    image: .init(systemName: "apple.logo"),
+                    title: .init("Sign in with Apple"),
+                    style: .primary) {
+                        AuthManager.shared.startSignInWithAppleFlow { error in
+                            isLogin = error == nil
+                        }
+                    }
+                ButtonView(
+                    image: .init("GoogleLogo"),
+                    title: .init("Sign in with Google"),
+                    style: .primary) {
+                        AuthManager.shared.startSignInWithGoogleId { error in
+                            isLogin = error == nil
+                        }
+                    }
+                
+                ButtonView(
+                    image: .init(systemName: "person.fill.questionmark"),
+                    title: .init("Sign in with anonymous"),
+                    style: .secondary, onclick: {
+                        AuthManager.shared.startSignInAnonymously { error in
+                            isLogin = error == nil
+                        }
+                    })
+            }
             
-        }.padding()
+        }
+        .padding()
+        .onAppear {
+            isLogin = AuthManager.shared.auth.currentUser != nil
+        }
     }
 }
 
